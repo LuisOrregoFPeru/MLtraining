a7da4d4405bff924246f6942f51cb3ed2f610539
LuisOrregoFPeru <51763524+LuisOrregoFPeru@users.noreply.github.com>
2025-05-19 16:14:00 -0500

    Update ce.py

--- ce.py
+++ ce.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import io
 
 # ---------------------------------------------------------
 # SUITE COMPLETA DE EVALUACIONES ECONÓMICAS EN SALUD – Versión 1.2
