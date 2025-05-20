8b2be01f4eec1b385e5b1b2107160a38e9caa80d
LuisOrregoFPeru <51763524+LuisOrregoFPeru@users.noreply.github.com>
2025-05-19 22:17:56 -0500

    Update ce.py

--- ce.py
+++ ce.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import io
 import matplotlib.ticker as mticker
+import math
 
 # ---------------------------------------------------------
 # SUITE COMPLETA DE EVALUACIONES ECONÓMICAS EN SALUD – Versión 1.2
@@ -199,10 +200,12 @@ elif analisis.startswith("2️⃣"):
         uptake_list.append(pct)
 
     # 5. Cálculos por año
-    uso_nueva  = [casos_anio * pct/100 for pct in uptake_list]
-    uso_actual = [casos_anio - un for un in uso_nueva]
-    cost_inc   = [delta * un for un in uso_nueva]
-    acumulado  = np.cumsum(cost_inc)
+
+     # Redondear siempre hacia arriba a entero
+     uso_nueva  = [math.ceil(casos_anio * pct/100) for pct in uptake_list]
+     uso_actual = [casos_anio - un for un in uso_nueva]
+     cost_inc   = [delta * un for un in uso_nueva]
+     acumulado  = np.cumsum(cost_inc)
 
     # 6. Construir DataFrame con Impacto en el PIM por año
     df = pd.DataFrame({
